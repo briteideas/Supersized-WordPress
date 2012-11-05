2efb9bbfb981ba21ffd2a0af6e90c74184ced80d
Alexander Sapountzis <asap@briteideas.com>
2012-11-05 00:23:01 -0500

    Adjusting styling for padding on top and bottom

--- supersized/plugins/supersized/supersized.php
+++ supersized/plugins/supersized/supersized.php
@@ -69,8 +69,8 @@ class Supersized{
                                 min_height              :   0,          // Min height allowed (in pixels)
                                 vertical_center         :   1,          // Vertically center background
                                 horizontal_center       :   1,          // Horizontally center background
-                                fit_always              :   0,          // Image will never exceed browser width or height (Ignores min. dimensions)
-                                fit_portrait            :   1,          // Portrait images will not exceed browser height
+                                fit_always              :   1,          // Image will never exceed browser width or height (Ignores min. dimensions)
+                                fit_portrait            :   0,          // Portrait images will not exceed browser height
                                 fit_landscape           :   0,          // Landscape images will not exceed browser width   
 
                                 $slides
